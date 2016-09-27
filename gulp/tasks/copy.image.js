2a998bf54ec6ae8f403ce9087c34e87f1a5c1923
Elena Malyugina <malyugina.el@yandex.ru>
2016-09-27 17:51:40 +0300

    сверстана первая страница

--- gulp/tasks/copy.image.js
+++ gulp/tasks/copy.image.js
@@ -2,7 +2,7 @@
 
 module.exports = function() {
   $.gulp.task('copy:image', function() {
-    return $.gulp.src('./source/images/images_simple/*.*', {since: $.gulp.lastRun('copy:image')})
+    return $.gulp.src('./source/images/images_simple/*/*.*', {since: $.gulp.lastRun('copy:image')})
       .pipe($.gulp.dest($.config.root + '/assets/img'));
   });
 };
