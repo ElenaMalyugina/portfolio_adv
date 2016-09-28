b5786ab13b66b42899c113796db62e1ccf0a3aaf
Elena Malyugina <malyugina.el@yandex.ru>
2016-09-28 17:49:10 +0300

    изменена сборка спрайта+подключен спрайт

--- gulp/tasks/sprite_png.js
+++ gulp/tasks/sprite_png.js
@@ -4,7 +4,8 @@ module.exports = function(){
 	$.gulp.task('sprite_png', function(){
  	var spriteData = $.gulp.src('././source/images/images_for_sprite/*.png').pipe($.gp.spritesmith({
     	imgName: 'sprite.png',
-    	cssName: 'sprite.scss'
+    	cssName: 'sprite.scss',
+    	padding: 50
   }));
   return spriteData.img.pipe($.gulp.dest('././build/assets/img')),
 	  spriteData.css.pipe($.gulp.dest('././source/style'));
