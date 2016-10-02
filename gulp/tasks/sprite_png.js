3c2fcb60ba1ca2aabc6d7756ce95f8c2de62ffe9
Elena Malyugina <malyugina.el@yandex.ru>
2016-10-03 01:04:22 +0300

    Дальнейшие улучшения

--- gulp/tasks/sprite_png.js
+++ gulp/tasks/sprite_png.js
@@ -5,6 +5,8 @@ module.exports = function(){
  	var spriteData = $.gulp.src('././source/images/images_for_sprite/*.png').pipe($.gp.spritesmith({
     	imgName: 'sprite.png',
     	cssName: 'sprite.scss',
+		cssFormat: 'css',
+		imgPath: '../img/sprite.png',
     	padding: 50
   }));
   return spriteData.img.pipe($.gulp.dest('././build/assets/img')),
